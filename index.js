d575e886bab734fe84ee82d022ed6aa1472d2f09
mostafizurRahaman <fahim654326@gmail.com>
2022-10-26 01:06:07 +0600

    cors install and used

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express(); 
+const cors = require('cors'); 
 const port = process.env.PORT || 5000; 
+app.use(cors());
 
 app.get('/', (req, res)=>{
    res.send('Web Learning sever in running now')
