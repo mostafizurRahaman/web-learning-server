09ee5e9b6f9a482b93a4d3641478fc428a0f095d
mostafizurRahaman <fahim654326@gmail.com>
2022-10-26 01:15:48 +0600

    courses api is created

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express(); 
 const cors = require('cors'); 
 const port = process.env.PORT || 5000; 
+const courses = require('./data/courses.json')
 app.use(cors());
 
 app.get('/', (req, res)=>{
    res.send('Web Learning sever in running now')
 })
 
+app.get('/courses', (req,res)=>{
+   res.send(courses);
+})
+
 
 app.listen(port , ()=>{
-   console.log(`Server is running on port : ${port}`)
+   console.log(`Server is running on port : ${port}`); 
 })
 
 
